a19cff3fd7cb7fde04fe0ffc829349ef94603015
appdev <>
2021-04-14 11:28:28 -0400

    add sort_selection

--- algoritm.py
+++ algoritm.py
@@ -14,10 +14,30 @@ def binary_search(s_list, item):
     return None
 
 
+def find_smallest(arr):
+    """Функция поискаа наименьшего элемента массива"""
+    smalllest = arr[0]
+    smalllest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smalllest:
+            smalllest = arr[i]
+            smalllest_index = i
+    return smalllest_index
+
+def selection_sort(arr):
+    """Функция сортировки выбором"""
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
 if __name__ == '__main__':
-    # spisok = [3, 3, 45, 6, 67, 8, 4, 34, 23, 6, 6, 7, 8]
-    with open('Юла телефоны.txt', 'r', encoding='utf-8') as file:
-        target_list = file.readlines()
-    h = list(set(target_list))
-    h.sort()
-    print('Позиция в списке {}'.format(binary_search(h, 79271201015)))
+    spisok = [3, 3, 45, 6, 67, 8, 4, 34, 23, 6, 6, 7, 8]
+    # with open('Юла телефоны.txt', 'r', encoding='utf-8') as file:
+    #     target_list = file.readlines()
+    # h = list(set(target_list))
+    # h.sort()
+    # print('Позиция в списке {}'.format(binary_search(h, 79271201015)))
+    print(selection_sort(spisok))
