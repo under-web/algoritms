fc15cbf3848d04838b1ffa88d24691709666097d
appdev <>
2021-05-09 15:48:48 +0300

    add Dextra

--- algoritm.py
+++ algoritm.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 def binary_search(s_list, item):
     """Бинарный поиск по отсортированному списку O(log n)"""
     low = 0
@@ -47,13 +48,15 @@ def quick_sort(array):
 
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
+
 def person_is_seller(name):
     return name[-1] == 'm'
+
 
 def algo_graphs(name):
     """ Функция реализующая графы"""
 
-    graph ={}
+    graph = {}
     search_queue = deque()
     search_queue += graph[name]
     searcher = []
@@ -69,6 +72,55 @@ def algo_graphs(name):
     return False
 
 
+# алгоритм Декстры со взвешенными графами
+graph = {}
+
+graph['start'] = {}
+graph['start']['a'] = 6
+graph['start']['b'] = 2
+
+graph['a'] = {}
+graph['a']['fin'] = 1
+graph['b'] = {}
+graph['b']['fin'] = 5
+graph['fin'] = {}
+
+infinity = float('inf')
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['in'] = None
+
+processed = []
+
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:  # перебрать все узлы
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:  # если это узел с наименьшей стоимостью из виденных и он еще не был обработан
+            lowest_cost = cost  # ...он назначается новым узлом с наименьшей стоимостью
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+node = find_lowest_cost_node(costs)  # найти узел с наименьшей стоимостью среди необработанных
+while node is not None:  # если обработаны все узлы, цикл while завершен
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():  # перебрать всех соседей текущего узла
+        new_cost = cost + neighbors[n]  # если к соседу можно быстрее
+        if costs[n] > new_cost:  # добраться через текущий узел...
+            costs[n] = new_cost  # обновить стоимость для этого узла
+            parents[n] = node  # этот узел становится новым родителем для соседа
+    processed.append(node)  # узел помечается как обработанный
+    node = find_lowest_cost_node(costs)  # найти следующий узел для обработки и повторить цикл
 
 if __name__ == '__main__':
     spisok = [3, 3, 45, 6, 67, 8, 4, 34, 23, 6, 6, 7, 8]
